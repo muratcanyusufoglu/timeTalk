517b492872270961c42f170e273ea650fa0fef5a
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2023-03-19 22:36:49 +0300

    Login page style upgraded

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -11,7 +11,8 @@ import React, {useEffect, useState} from 'react';
 import storage from '../storage/storage';
 import axios from 'axios';
 import Config from 'react-native-config';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const window = Dimensions.get('window');
 export default function Profile() {
@@ -21,6 +22,7 @@ export default function Profile() {
   const [data, setData] = useState([]);
   const [followers, setFollowers] = useState([]);
   const [refresh, setRefresh] = useState<boolean>();
+  const [index, setIndex] = useState<number>(1);
 
   const userData: {
     follower: string;
@@ -63,16 +65,64 @@ export default function Profile() {
           style={styles.profileImage}
           resizeMode="cover"
         />
-        <Text>{data.name}</Text>
+        <View>
+          <Text style={styles.userNameText}>{data.name}</Text>
+        </View>
       </View>
-      {/* <FlatList
-        extraData={followers}
-        data={followers}
-        refreshing={refresh}
-        numColumns={1}
-        style={{}}
-        renderItem={({item}) => <InsideFlatlist item={item} />}
-      /> */}
+      <View style={styles.altTextsView}>
+        <TouchableOpacity
+          style={
+            index == 1
+              ? styles.altBarsButtonsActive
+              : styles.altBarsButtonsInactive
+          }
+          onPress={() => setIndex(1)}>
+          <Text style={styles.userNameText}>Contents</Text>
+          <Icon name="camera-retro" size={15} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={
+            index == 2
+              ? styles.altBarsButtonsActive
+              : styles.altBarsButtonsInactive
+          }
+          onPress={() => setIndex(2)}>
+          <Text style={styles.userNameText}>Followers</Text>
+          <Icon name="user-alt" size={15} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={
+            index == 3
+              ? styles.altBarsButtonsActive
+              : styles.altBarsButtonsInactive
+          }
+          onPress={() => setIndex(3)}>
+          <Text style={styles.userNameText}>Following</Text>
+          <Icon name="users" size={15} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={
+            index == 4
+              ? styles.altBarsButtonsActive
+              : styles.altBarsButtonsInactive
+          }
+          onPress={() => setIndex(4)}>
+          <Text style={styles.userNameText}>Packages</Text>
+          <Icon name="box-open" size={15} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.line} />
+
+      {index == 1 ? (
+        <FlatList
+          extraData={followers}
+          data={followers}
+          refreshing={refresh}
+          numColumns={1}
+          style={{}}
+          renderItem={({item}) => <InsideFlatlist item={item} />}
+        />
+      ) : null}
     </SafeAreaView>
   );
 }
@@ -148,8 +198,34 @@ const styles = StyleSheet.create({
     borderColor: 'transparent',
   },
   userInfos: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  userNameText: {
+    marginTop: 10,
+  },
+  altTextsView: {
+    margin: 0,
     flexDirection: 'row',
     alignItems: 'center',
-    marginHorizontal: 10,
+    justifyContent: 'space-evenly',
+    marginTop: window.height / 25,
+  },
+  altBarsButtonsActive: {
+    backgroundColor: '#E0ECFF',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 8,
+  },
+  altBarsButtonsInactive: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 8,
+  },
+  line: {
+    margin: 10,
+    borderWidth: 0.2,
   },
 });
